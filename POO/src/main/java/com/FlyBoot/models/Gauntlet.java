b3a2dff545a6f6db698651b90481d54ea5c906e7
IMENDOZA <imendoza@unab.edu.co>
2023-06-28 20:12:19 -0500

    Ejercicio IoC ajuste imports

--- POO/src/main/java/com/FlyBoot/models/Gauntlet.java
+++ POO/src/main/java/com/FlyBoot/models/Gauntlet.java
@@ -1,6 +1,5 @@
 package com.FlyBoot.models;
 
-import com.FlyBoot.models.gems.AbstractGem;
 import com.FlyBoot.models.gems.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
