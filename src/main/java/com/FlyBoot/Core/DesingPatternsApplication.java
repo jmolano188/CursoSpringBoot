23208bdedda0f45fe13f33de13a4d850a59061ca
IMENDOZA <imendoza@unab.edu.co>
2023-08-12 18:34:39 -0500

    Reestructuracion proyecto

--- src/main/java/com/FlyBoot/Core/DesingPatternsApplication.java
+++ src/main/java/com/FlyBoot/Core/DesingPatternsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CursoSpringApplication {
+public class DesingPatternsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CursoSpringApplication.class, args);
+		SpringApplication.run(DesingPatternsApplication.class, args);
 	}
 
 }
--- src/main/java/com/FlyBoot/Core/utilities/PagesUtil.java
+++
@@ -1,6 +0,0 @@
-package com.FlyBoot.Core.utilities;
-
-public class PagesUtil {
-    public static final String HOME = "index";
-    public static final String POST = "post";
-}
