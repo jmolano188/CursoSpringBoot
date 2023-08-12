7d7206986b4805d4bb323bdad0b51997f37dd2bc
IMENDOZA <imendoza@unab.edu.co>
2023-08-12 18:30:38 -0500

    Clase 8: Adicion de vista post, estilos css y modelo post

--- src/main/java/com/FlyBoot/Core/controllers/BasicController.java
+++ src/main/java/com/FlyBoot/Core/controllers/BasicController.java
@@ -1,10 +1,13 @@
 package com.FlyBoot.Core.controllers;
 
 import com.FlyBoot.Core.models.Post;
+import com.FlyBoot.Core.utilities.PagesUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,8 +30,20 @@ public class BasicController {
     }
     @GetMapping("/post")
     public String getPost(Model model){
-        model.addAttribute("post", this.getPost());
+        model.addAttribute("posts", this.getPost());
         return "index";
+    }
+    @GetMapping("/public")
+    public Object listPosts(){
+        ModelAndView response = new ModelAndView(PagesUtil.HOME);
+        response.addObject("posts", this.getPost());
+        return response;
+    }
+    @GetMapping()
+    public Object getPost(@RequestParam(defaultValue = "1", name = "id", required = false) int id){
+        ModelAndView response = new ModelAndView(PagesUtil.POST);
+        response.addObject("post", this.getPost().get(id));
+        return response;
     }
 
 }
---
+++ src/main/java/com/FlyBoot/Core/utilities/PagesUtil.java
@@ -0,0 +1,6 @@
+package com.FlyBoot.Core.utilities;
+
+public class PagesUtil {
+    public static final String HOME = "index";
+    public static final String POST = "post";
+}
