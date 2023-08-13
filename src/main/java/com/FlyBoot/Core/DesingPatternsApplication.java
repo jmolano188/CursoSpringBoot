36e78131196436acc7ff9dff127bb6b4f491dd48
IMENDOZA <imendoza@unab.edu.co>
2023-08-12 19:55:00 -0500

    ADD - Patron singleton

--- src/main/java/com/FlyBoot/Core/DesingPatternsApplication.java
+++ src/main/java/com/FlyBoot/Core/DesingPatternsApplication.java
@@ -1,13 +1,50 @@
 package com.FlyBoot.Core;
 
-import org.springframework.boot.SpringApplication;
+import com.FlyBoot.Core.singleton.Credenciales;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DesingPatternsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DesingPatternsApplication.class, args);
+		/*
+		System.out.println("INICIO HILO UNICO");
+		Credenciales credenciales = Credenciales.getInstance("IMENDOZA", "ADMIN");
+		Credenciales credenciales2 = Credenciales.getInstance("JMOLANO", "SUPREMO");
+		System.out.println(credenciales.toString());
+		System.out.println(credenciales2);
+		System.out.println("FIN HILO UNICO");
+		 */
+		System.out.println("------------------");
+		System.out.println("INICIO MULTIHILO");
+		Thread hilo1 = new Thread(new CredencialesIvan());
+		Thread hilo2 = new Thread(new CredencialesMolano());
+		hilo1.start();
+		hilo2.start();
+		System.out.println("FIN MULTIHILO");
 	}
+
+	/**
+	 * Creacion de multihilos para notar el fallo en el patron
+	 * singleton cuando no se tiene control en los hilos
+	 */
+	static class CredencialesIvan implements Runnable{
+
+		@Override
+		public void run() {
+			Credenciales credenciales = Credenciales.getInstance("IMENDOZA", "ADMIN");
+			System.out.println(credenciales);
+		}
+	}
+
+	static class CredencialesMolano implements Runnable{
+
+		@Override
+		public void run() {
+			Credenciales credenciales = Credenciales.getInstance("JMOLANO", "SUPREMO");
+			System.out.println(credenciales);
+		}
+	}
+
 
 }
---
+++ src/main/java/com/FlyBoot/Core/singleton/Credenciales.java
@@ -0,0 +1,85 @@
+package com.FlyBoot.Core.singleton;
+
+import java.util.Objects;
+
+/**
+ * Singleton
+ * @author IMENDOZA
+ */
+public class Credenciales {
+    // The field must be declared volatile so that double check lock would work
+    // correctly. Esto se usa para solucion al multihilo
+    private static volatile Credenciales instance;
+    private final String user;
+    private final String pwd;
+
+    private Credenciales(String user, String pwd) {
+        // The following code emulates slow initialization.
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        this.user = user;
+        this.pwd = pwd;
+    }
+
+    /* //Metodo que se implemento para el hilo unico
+    public static Credenciales getInstance(String user, String pwd){
+        if(instance == null){
+            instance = new Credenciales(user, pwd);
+        }
+        return instance;
+    }
+     */
+
+    // The approach taken here is called double-checked locking (DCL). It
+    // exists to prevent race condition between multiple threads that may
+    // attempt to get singleton instance at the same time, creating separate
+    // instances as a result.
+    //
+    // It may seem that having the `result` variable here is completely
+    // pointless. There is, however, a very important caveat when
+    // implementing double-checked locking in Java, which is solved by
+    // introducing this local variable.
+    //
+    // You can read more info DCL issues in Java here:
+    // https://refactoring.guru/java-dcl-issue
+
+    /**
+     * Metodo que da solucion al asincronismo en los multihilos
+     */
+    public static Credenciales getInstance(String user, String pwd){
+        Credenciales result = instance;
+        if (result != null) {
+            return result;
+        }
+        synchronized (Credenciales.class){
+            if(instance == null){
+                instance = new Credenciales(user, pwd);
+            }
+            return instance;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Credenciales that = (Credenciales) o;
+        return Objects.equals(user, that.user) && Objects.equals(pwd, that.pwd);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(user, pwd);
+    }
+
+    @Override
+    public String toString() {
+        return "Credenciales{" +
+                "user='" + user + '\'' +
+                ", pwd='" + pwd + '\'' +
+                '}';
+    }
+}
